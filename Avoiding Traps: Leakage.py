7f424f7563d046e60cc727e73436feffa1480a28
rajmohan <rajmohan.mech05@gmail.com>
2026-04-15 20:34:10 +0400

    Update Avoiding Traps: Leakage.py

--- Avoiding Traps: Leakage.py
+++ Avoiding Traps: Leakage.py
@@ -3,17 +3,13 @@ from sklearn.preprocessing import StandardScaler
 from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import train_test_split
 
-# Data
 X, y = make_classification(n_samples=1000, n_features=10, random_state=42)
 
-# ❌ WRONG: scaling before split
 scaler = StandardScaler()
 X_scaled = scaler.fit_transform(X)
 
-# Split
 X_train, X_test, y_train, y_test = train_test_split(X_scaled, y, test_size=0.2, random_state=42)
 
-# Model
 model = LogisticRegression()
 model.fit(X_train, y_train)
 
